847f8f1c0b3826843016a29bdc80fbeebe2bf1f7
leykun-gizaw <leykun-gizaw@users.noreply.github.com>
2022-12-03 11:07:33 +0000

    deploy: c0633a9379dec9efd9c2d27102c121852c6f73bc

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const upvoteApp = {
       submissions: Seed.submissions,
     };
   },
+  methods: {
+    upVote(submission) {
+      const sub = this.submissions.find((s) => s === submission);
+      sub.votes++;
+    },
+  },
   computed: {
     sortedSubmissions() {
       return this.submissions.sort((sub1, sub2) => sub2.votes - sub1.votes);
